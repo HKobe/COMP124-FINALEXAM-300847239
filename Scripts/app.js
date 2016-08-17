d31b137f872aec7281afe96473578159a8037e9b
HKobe <HKobeissi1@hotmail.com>
2016-08-17 13:32:16 -0400

    Bitmap added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,21 +1,39 @@
+/*Hassan Kobeissi - 300847239
+Date Modified : 2016-08-17
+Date Created: 2016-08-17
+*/
 var core;
 (function (core) {
     "use strict";
     var canvas;
     var stage;
+    //Button Variable   
+     var rollButton;
+
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
         stage = new createjs.Stage(canvas);
         stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60;
+        //Creating the button with createjs
+       var rollButton = new createjs.Bitmap("Assets\images\rollButton.png");
+       
         createjs.Ticker.on("tick", gameLoop);
         main();
     }
     function gameLoop() {
         stage.update();
     }
+    //Rolls the Dice
+    function rollDice() {
+
+        var random = Math.random(0,6);
+    }
     function main() {
+        //Adding to the stage
+       stage.addChild(rollButton);
+
     }
     window.addEventListener("load", init);
 })(core || (core = {}));
