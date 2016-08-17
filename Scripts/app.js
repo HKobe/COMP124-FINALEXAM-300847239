6fbc4ba7e3b03c0b6af56cf569a535ec77f51b8f
HKobe <HKobeissi1@hotmail.com>
2016-08-17 14:16:05 -0400

    Final Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -22,36 +22,37 @@ var core;
         main();
     }
     function gameLoop() {
-        var x = 81;
-        var y = 100;
+
         stage.update();
     }
     //Rolls the Dice
     function rollDice() {
 
         var random = Math.random(0,6);
+        var random2= Math.floor(0,6);
     }
     function main() {
         //Adding the Image
         
         var blank = new createjs.Bitmap("Assets/images/blank.png");
         var dice = new createjs.Bitmap("Assets/images/blank.png");
-        var Dice1 = new createjs.Bitmap("../Assets/images/1.png");
-        var Dice2 = new createjs.Bitmap("../Assets/images/2.png");
-        var Dice3 = new createjs.Bitmap("../Assets/images/3.png");
-        var Dice4 = new createjs.Bitmap("../Assets/images/4.png");
-        var Dice5 = new createjs.Bitmap("../Assets/images/5.png");
-        var Dice6 = new createjs.Bitmap("../Assets/images/6.png");
+       /* var Dice1 = new createjs.Bitmap("Assets/images/1.png");
+        var Dice2 = new createjs.Bitmap("Assets/images/2.png");
+        var Dice3 = new createjs.Bitmap("Assets/images/3.png");
+        var Dice4 = new createjs.Bitmap("Assets/images/4.png");
+        var Dice5 = new createjs.Bitmap("Assets/images/5.png");
+        var Dice6 = new createjs.Bitmap("Assets/images/6.png");*/
          var rollButton = new createjs.Bitmap("Assets/images/rollButton.png");
 
         //Adding to the stage
        stage.addChild(rollButton);
-        stage.addChild(Dice1);
+      /*  stage.addChild(Dice1);
         stage.addChild(Dice2);
         stage.addChild(Dice3);
         stage.addChild(Dice4);
         stage.addChild(Dice5);
-        stage.addChild(Dice6);
+        stage.addChild(Dice6);*/
+
         //Blanks
        stage.addChild(blank);
        stage.addChild(dice);
